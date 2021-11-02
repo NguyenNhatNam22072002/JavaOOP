93117ecf6d7f9468fbe1d17840ffeac20a94aa6d
ZLoganZ <ln26805@gmail.com>
2021-11-02 10:39:50 +0700

    Tính đóng gói

--- OOP/src/Program.java
+++ OOP/src/Program.java
@@ -2,7 +2,7 @@ public class Program {
     public static void main(String[] args) {
         People P1 = new People();
         P1.setName("Hai");
-        P1.setTuoi(0);
+        P1.setTuoi(19);
         System.out.println(P1.Name());
         System.out.println(P1.Tuoi());
     }
