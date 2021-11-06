6f40deeb01a7fa6ad4d439329e4558fcce88b9a4
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-07 00:28:14 +0700

    Add Character.java file

---
+++ HELLOWORLD/src/Character.java
@@ -0,0 +1,27 @@
+public class Character
+{
+		private int level;
+        private String name;
+        public Character()
+        {
+        	
+        }
+        public Character(String name)
+        {
+            this.name = name;
+        }
+        public Character(int level, String name)
+        {
+        	this(name);
+        	this.level = level;
+        }
+        public int getLevel()
+        {
+        	return this.level;
+        }
+        public void setLevel(int level)
+        {
+        	if(level >= 0) this.level = level;
+        	else System.out.print("Level must be > 0");
+        }
+}
