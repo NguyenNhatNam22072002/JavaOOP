8ee0ec3f9e4b403d6752191f45371bb0ed9f2034
ZLoganZ <ln26805@gmail.com>
2021-11-06 18:54:19 +0700

    Set code

--- OOP/src/Program.java
+++ OOP/src/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         People P1 = new People();
-        P1.setName("Hai");
+        P1.setName("LoganZ");
         P1.setTuoi(19);
         System.out.println(P1.Name());
         System.out.println(P1.Tuoi());
