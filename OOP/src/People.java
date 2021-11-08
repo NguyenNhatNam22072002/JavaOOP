9e88dd10760462a21dab5b0af6bb740dc3bca6f1
ZLoganZ <ln26805@gmail.com>
2021-11-08 16:50:43 +0700

    Update Code

--- OOP/src/People.java
+++ OOP/src/People.java
@@ -1,11 +1,9 @@
 public class People {
     private int _tuoi;
     private String _name;
-    public void setName(String N) {
-        this._name = N;
-    }
-    public void setTuoi(int T) {
-        this._tuoi = T;
+    public void SetInfo(String name, int tuoi) {
+        this._name = name;
+        this._tuoi = tuoi;
     }
     public void PrintInfo(){
         System.out.println("Ho ten: "+this._name);
--- OOP/src/Program.java
+++ OOP/src/Program.java
@@ -1,8 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         People P1 = new People();
-        P1.setName("Nguyen Hai");
-        P1.setTuoi(19);
+        P1.SetInfo("Nguyen Hai", 19);
         P1.PrintInfo();
     }
 }
