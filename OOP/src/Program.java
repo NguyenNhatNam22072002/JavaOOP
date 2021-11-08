2242c449acdb32586c0aea9cf16bd7cb8c34d3cc
ZLoganZ <ln26805@gmail.com>
2021-11-08 16:48:37 +0700

    Update Code

--- OOP/src/Program.java
+++ OOP/src/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         People P1 = new People();
-        P1.setName("LoganZ");
+        P1.setName("Nguyen Hai");
         P1.setTuoi(19);
         P1.PrintInfo();
     }
