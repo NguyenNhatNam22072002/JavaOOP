b659924d89eeb4eec712010ade80446e9a3f2c75
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-19 21:14:53 +0700

    Update class Student

--- Inheritance/Student.java
+++ Inheritance/Student.java
@@ -16,8 +16,18 @@ public class Student extends Person{
     public void setGpa(double gpa){
         this.gpa = gpa;
     }
+   
+    public void Printname(){
+        System.out.println("Namedxsxscscsc :");
+    }
 
     public void Printgpa(){
         System.out.println("GPA :" + gpa);
     }
+    
+    public void NAM(int n)
+    {
+        System.out.println("CON");
+    }
+    int n = 2;
 }
