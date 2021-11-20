ce95f674353556f836d21d11e086955edea3ef77
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-20 17:37:31 +0700

    Add class Main

---
+++ Interface_Polymorphism/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+        DongVat[] dv = new DongVat[5];
+        dv[0] = new ConBo("Bo sua Vinamilk");
+        dv[1] = new ConDe("De nui");
+        dv[2] = new ConSoi("Soi xam");
+        dv[3] = new ConHo("Ho rang kiem");
+        dv[4] = new ConBo("Bo sua Ha Lan");
+        for (DongVat dongVat : dv) {            //Giới thiệu các con vật
+            System.out.println("Toi ten la: " + dongVat.getTen());
+            dongVat.an();
+            dongVat.keu();
+        }
+    }
+}
+
+
+
+
+
