8f10dae3f4c5780009c5634afffe2eb02c9bc59f
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-20 16:49:31 +0700

    Add class ConSoi

---
+++ Interface_Polymorphism/ConSoi.java
@@ -0,0 +1,15 @@
+public class ConSoi extends DVAnThit{
+    public ConSoi() 
+    {
+        super();
+    }
+    public ConSoi(String t)
+    {
+        super(t);
+    }
+    @Override
+    public  void keu()
+    {
+        System.out.println("Toi keu Hhuuu...!!!");
+    }
+}
