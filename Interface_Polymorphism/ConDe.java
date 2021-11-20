dddd84ebf4d19d949da2a5bec9efa2bc9eb744ee
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-20 16:03:49 +0700

    Add class ConDe

---
+++ Interface_Polymorphism/ConDe.java
@@ -0,0 +1,15 @@
+public class ConDe extends DVAnCo{
+    public ConDe() 
+    {
+        super();
+    }
+    public ConDe(String t)
+    {
+        super(t);
+    }
+    @Override
+    public  void keu()
+    {
+        System.out.println("Toi keu BBeee ... BBeee...!!!");
+    }
+}
