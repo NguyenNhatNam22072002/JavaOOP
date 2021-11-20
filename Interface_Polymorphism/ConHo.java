62e76d13e4dc2dee2d0f98cc3560f8eee30da343
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-20 16:49:00 +0700

    Add class ConHo

---
+++ Interface_Polymorphism/ConHo.java
@@ -0,0 +1,15 @@
+public class ConHo extends DVAnThit{
+    public ConHo() 
+    {
+        super();
+    }
+    public ConHo(String t)
+    {
+        super(t);
+    }
+    @Override
+    public  void keu()
+    {
+        System.out.println("Toi keu GGraooooo...!!!");
+    }
+}
