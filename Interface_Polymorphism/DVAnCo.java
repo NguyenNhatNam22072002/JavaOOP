3f21ee6237d860fe2bd1cc7188310f16b0a98e08
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-11-20 14:16:48 +0700

    Add class DVAnCo

---
+++ Interface_Polymorphism/DVAnCo.java
@@ -0,0 +1,15 @@
+public class DVAnCo extends DongVat{
+    public DVAnCo() 
+    {
+        super();
+    }
+    public DVAnCo(String t)
+    {
+        super(t);
+    }
+    @Override
+    public void an()
+    {
+        System.out.println("Toi an Co tuoi!!!");
+    }
+}
