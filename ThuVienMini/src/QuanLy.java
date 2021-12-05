c7502fb0b87f84e261b5b0bec82e069d0f3be35a
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-12-06 00:14:09 +0700

    Sua Code

--- ThuVienMini/src/QuanLy.java
+++ ThuVienMini/src/QuanLy.java
@@ -188,6 +188,83 @@ public class QuanLy {
                     sc.nextLine();
                     sc.nextLine();
                     break;
+                    case 3:
+                    System.out.println("\t\t    -------Nhap them sach-------");
+                    int nhap;
+                    do {
+                        System.out.print(
+                                "Loai sach muon them (SGK: chon 1/ Sach Tham khao: chon 2/ Sach khac: chon 3): ");
+                        int number = sc.nextInt();
+                        switch (number) {
+                            case 1:
+                                sc.nextLine();
+                                SachGiaoKhoa sachgk = new SachGiaoKhoa();
+                                sachgk.NhapSach();
+                                Boolean check1 = false;
+                                for (int i = 0; i < listsachgiaokhoa.size(); i++)
+                                    if (sachgk.getMaSach().equals(listsachgiaokhoa.get(i).getMaSach())) {
+                                        listsachgiaokhoa.get(i)
+                                                .setSoLuong(listsachgiaokhoa.get(i).getSoLuong() + sachgk.getSoLuong());
+                                        check1 = true;
+                                        break;
+                                    }
+                                if (check1 == false)
+                                    listsachgiaokhoa.add(sachgk);
+                                break;
+                            case 2:
+                                sc.nextLine();
+                                SachThamKhao sachtk = new SachThamKhao();
+                                sachtk.NhapSach();
+                                Boolean check2 = false;
+                                for (int i = 0; i < listsachthamkhao.size(); i++)
+                                    if (sachtk.getMaSach().equals(listsachthamkhao.get(i).getMaSach())) {
+                                        listsachthamkhao.get(i)
+                                                .setSoLuong(listsachthamkhao.get(i).getSoLuong() + sachtk.getSoLuong());
+                                        check2 = true;
+                                        break;
+                                    }
+                                if (check2 == false)
+                                    listsachthamkhao.add(sachtk);
+                                break;
+                            default:
+                                sc.nextLine();
+                                SachKhac sachkhac = new SachKhac();
+                                sachkhac.NhapSach();
+                                Boolean check3 = false;
+                                for (int i = 0; i < listsachkhac.size(); i++)
+                                    if (sachkhac.getMaSach().equals(listsachkhac.get(i).getMaSach())) {
+                                        listsachkhac.get(i)
+                                                .setSoLuong(listsachkhac.get(i).getSoLuong() + sachkhac.getSoLuong());
+                                        check3 = true;
+                                        break;
+                                    }
+                                if (check3 == false)
+                                    listsachkhac.add(sachkhac);
+                                break;
+                        }
+                        System.out.print("Ban co muon nhap sach nua khong (1-Yes/2-No):");
+                        nhap = sc.nextInt();
+                    } while (nhap == 1);
+                    break;
+                    case 4:
+                    System.out.println("\t\t    -------Dang ky the thu vien--------");
+                    TheThuVien t = new TheThuVien();
+                    t.Dangky();
+                    for (int i = 0; i < ds.size(); i++) {
+                        if (t.getMaThe().equals(ds.get(i).getMaThe())) {
+                            System.out.println("Ma the da co nguoi dang ky! Vui long ma khac!");
+                            System.out.print("Nhap lai ma: ");
+                            String mathe = sc.next();
+                            t.setMaThe(mathe);
+                            i -= 1;
+                        }
+                    }
+                    ds.add(t);
+                    System.out.println("Dang ky thanh cong!");
+                    System.out.print("Nhan Enter de tiep tuc!");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
                 case 5:
                     int s = 0;
                     do {
@@ -566,83 +643,6 @@ public class QuanLy {
                                 break;
                         }
                     }
-                    break;
-                case 4:
-                    System.out.println("\t\t    -------Dang ky the thu vien--------");
-                    TheThuVien t = new TheThuVien();
-                    t.Dangky();
-                    for (int i = 0; i < ds.size(); i++) {
-                        if (t.getMaThe().equals(ds.get(i).getMaThe())) {
-                            System.out.println("Ma the da co nguoi dang ky! Vui long ma khac!");
-                            System.out.print("Nhap lai ma: ");
-                            String mathe = sc.next();
-                            t.setMaThe(mathe);
-                            i -= 1;
-                        }
-                    }
-                    ds.add(t);
-                    System.out.println("Dang ky thanh cong!");
-                    System.out.print("Nhan Enter de tiep tuc!");
-                    sc.nextLine();
-                    sc.nextLine();
-                    break;
-                case 3:
-                    System.out.println("\t\t    -------Nhap them sach-------");
-                    int nhap;
-                    do {
-                        System.out.print(
-                                "Loai sach muon them (SGK: chon 1/ Sach Tham khao: chon 2/ Sach khac: chon 3): ");
-                        int number = sc.nextInt();
-                        switch (number) {
-                            case 1:
-                                sc.nextLine();
-                                SachGiaoKhoa sachgk = new SachGiaoKhoa();
-                                sachgk.NhapSach();
-                                Boolean check1 = false;
-                                for (int i = 0; i < listsachgiaokhoa.size(); i++)
-                                    if (sachgk.getMaSach().equals(listsachgiaokhoa.get(i).getMaSach())) {
-                                        listsachgiaokhoa.get(i)
-                                                .setSoLuong(listsachgiaokhoa.get(i).getSoLuong() + sachgk.getSoLuong());
-                                        check1 = true;
-                                        break;
-                                    }
-                                if (check1 == false)
-                                    listsachgiaokhoa.add(sachgk);
-                                break;
-                            case 2:
-                                sc.nextLine();
-                                SachThamKhao sachtk = new SachThamKhao();
-                                sachtk.NhapSach();
-                                Boolean check2 = false;
-                                for (int i = 0; i < listsachthamkhao.size(); i++)
-                                    if (sachtk.getMaSach().equals(listsachthamkhao.get(i).getMaSach())) {
-                                        listsachthamkhao.get(i)
-                                                .setSoLuong(listsachthamkhao.get(i).getSoLuong() + sachtk.getSoLuong());
-                                        check2 = true;
-                                        break;
-                                    }
-                                if (check2 == false)
-                                    listsachthamkhao.add(sachtk);
-                                break;
-                            default:
-                                sc.nextLine();
-                                SachKhac sachkhac = new SachKhac();
-                                sachkhac.NhapSach();
-                                Boolean check3 = false;
-                                for (int i = 0; i < listsachkhac.size(); i++)
-                                    if (sachkhac.getMaSach().equals(listsachkhac.get(i).getMaSach())) {
-                                        listsachkhac.get(i)
-                                                .setSoLuong(listsachkhac.get(i).getSoLuong() + sachkhac.getSoLuong());
-                                        check3 = true;
-                                        break;
-                                    }
-                                if (check3 == false)
-                                    listsachkhac.add(sachkhac);
-                                break;
-                        }
-                        System.out.print("Ban co muon nhap sach nua khong (1-Yes/2-No):");
-                        nhap = sc.nextInt();
-                    } while (nhap == 1);
                     break;
                 default:
                     System.out.println("\t\tTam biet!!!!!");
