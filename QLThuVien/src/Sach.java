aaa92cde35fd14b778af5e606c2d1f023e4258fb
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2021-12-20 21:44:01 +0700

    Sua code

--- QLThuVien/src/Sach.java
+++ QLThuVien/src/Sach.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Sach {
@@ -70,75 +68,4 @@ public abstract class Sach {
     }
 
     public abstract List<String> InSach();
-
-    public static void NhapThemSach(ArrayList<Sach> thuvien, ArrayList<SachGiaoKhoa> listsachgiaokhoa, ArrayList<SachThamKhao> listsachthamkhao, ArrayList<SachKhac> listsachkhac)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\t\t    -------Nhap them sach-------");
-                    int nhap;
-                    do {
-                        System.out.print(
-                                "Loai sach muon them (SGK: chon 1/ Sach Tham khao: chon 2/ Sach khac: chon 3): ");
-                        int number; 
-                        number = sc.nextInt();
-                        switch (number) {
-                            case 1:
-                                SachGiaoKhoa sachgk = new SachGiaoKhoa();
-                                sachgk.NhapSach();
-                                Boolean check1 = false;     //Cho sách vừa tạo chưa có trong listsach
-                                for (int i = 0; i < listsachgiaokhoa.size(); i++)       //Kiểm tra sách vừa tạo đã có chưa
-                                    if (sachgk.getMaSach().equals(listsachgiaokhoa.get(i).getMaSach()))     //Nếu đã có
-                                    {
-                                        listsachgiaokhoa.get(i).setSoLuong(listsachgiaokhoa.get(i).getSoLuong() + sachgk.getSoLuong()); //Set lại số lượng sách đó
-                                        check1 = true;  //Cho sách vừa tạo đã có
-                                        break;
-                                    }
-                                
-                                if (check1 == false)    //Nếu sách vừa tạo chưa có 
-                                {
-                                    thuvien.add(sachgk);    //Thêm sách mới tạo vào listsach
-                                    listsachgiaokhoa.add(sachgk);   //Thêm sách mới tạo vào list thư viện
-                                }
-                                break;
-                            case 2:
-                                SachThamKhao sachtk = new SachThamKhao();   
-                                sachtk.NhapSach();
-                                Boolean check2 = false;     //Cho sách vừa tạo chưa có trong listsach
-                                for (int i = 0; i < listsachthamkhao.size(); i++)      //Kiểm tra sách vừa tạo đã có chưa
-                                    if (sachtk.getMaSach().equals(listsachthamkhao.get(i).getMaSach()))     //Nếu đã có
-                                    {   
-                                        listsachthamkhao.get(i).setSoLuong(listsachthamkhao.get(i).getSoLuong() + sachtk.getSoLuong());     //Set lại số lượng sách đó
-                                        check2 = true;  //Cho sách vừa tạo đã có
-                                        break;
-                                    }
-                                if (check2 == false)
-                                {
-                                    thuvien.add(sachtk);    //Thêm sách mới tạo vào listsach
-                                    listsachthamkhao.add(sachtk);   //Thêm sách mới tạo vào list thư viện
-                                }
-                                break;
-                            case 3:
-                                SachKhac sachkhac = new SachKhac();
-                                sachkhac.NhapSach();
-                                Boolean check3 = false;     //Cho sách vừa tạo chưa có trong listsach
-                                for (int i = 0; i < listsachkhac.size(); i++)       //Kiểm tra sách vừa tạo đã có chưa
-                                    if (sachkhac.getMaSach().equals(listsachkhac.get(i).getMaSach()))       //Nếu đã có
-                                    {
-                                        listsachkhac.get(i).setSoLuong(listsachkhac.get(i).getSoLuong() + sachkhac.getSoLuong());       //Set lại số lượng sách đó
-                                        check3 = true;  //Cho sách vừa tạo đã có
-                                        break;
-                                    }
-                                if (check3 == false)
-                                {
-                                    thuvien.add(sachkhac);      //Thêm sách mới tạo vào listsach
-                                    listsachkhac.add(sachkhac);     //Thêm sách mới tạo vào list thư viện
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                        System.out.print("Ban co muon nhap sach nua khong (1-Yes/2-No):");
-                        nhap = sc.nextInt();
-                    } while (nhap == 1);
-    }
 }
--- QLThuVien/src/SachGiaoKhoa.java
+++ QLThuVien/src/SachGiaoKhoa.java
@@ -34,34 +34,4 @@ public class SachGiaoKhoa extends Sach {
     public List<String> InSach() {
         return Arrays.asList(this.maSach, this.tenSach, this.tenNXB, String.valueOf(this.lop),String.valueOf(this.soluong));
     }
-    public static void ThongtinSach(ArrayList<SachGiaoKhoa> listsachgiaokhoa)
-    {
-        System.out.println("\t\t    ---------Thong tin SGK-----------");
-                    if (listsachgiaokhoa.size() == 0) {
-                        System.out.println("\n\t\t\t    Khong co sach!\n");
-                    } else {
-                        List<String> headersList = Arrays.asList("Ma sach", "Ten sach", "Nha xuat ban", "Phan lop",
-                                "So luong");
-                        List<List<String>> rowsList = new ArrayList<List<String>>();
-                        for (int i = 0; i < listsachgiaokhoa.size(); i++) {
-                            rowsList.add(listsachgiaokhoa.get(i).InSach());
-                        }
-                        Board board = new Board(75);
-                        Table table = new Table(board, 75, headersList, rowsList);
-                        List<Integer> colAlignList = Arrays.asList(
-                                Block.DATA_CENTER,
-                                Block.DATA_CENTER,
-                                Block.DATA_CENTER,
-                                Block.DATA_CENTER,
-                                Block.DATA_CENTER);
-                        List<Integer> colWidthsListEdited = Arrays.asList(10, 24, 13, 12, 10);
-                        table.setColAlignsList(colAlignList).setColWidthsList(colWidthsListEdited)
-                                .setGridMode(Table.GRID_FULL);
-                        Block tableBlock = table.tableToBlocks();
-                        board.setInitialBlock(tableBlock);
-                        board.build();
-                        String tableString = board.getPreview();
-                        System.out.println(tableString);
-                    }
-    }
 }
